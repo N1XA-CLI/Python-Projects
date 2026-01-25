eaa80123176c3e2d1ee392d30e32a26b880c534c
N1XA-CLI <prasadvikram094@gmail.com>
2026-01-25 09:45:21 +0530

    Progressed

--- Pong_Game/main.py
+++ Pong_Game/main.py
@@ -1,10 +1,29 @@
 from turtle import Turtle, Screen
+from paddel import Paddel
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong Game")
+screen.tracer(0)
 
+paddel_left = Paddel()
+paddel_left.goto(350, 0)
+
+paddel_right = Paddel()
+paddel_right.goto(-350, 0)
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    
+    screen.listen()
+    screen.onkey(paddel_right.go_up, "Up")
+    screen.onkey(paddel_right.go_down, "Down")
+
+    screen.onkey(paddel_left.go_up, "w")
+    screen.onkey(paddel_left.go_down, "s")
 
 
 screen.exitonclick()
---
+++ Pong_Game/paddel.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+
+class Paddel(Turtle):
+
+    def __init__(self):
+        """Creates a paddel."""
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        
+    def go_up(self):
+        """Vertically moves the paddel up by 20px."""
+        self.goto(self.xcor(), self.ycor() + 20)
+        
+    def go_down(self):
+        """Vertically moves the paddel down by 20px."""
+        self.goto(self.xcor(), self.ycor() - 20)
