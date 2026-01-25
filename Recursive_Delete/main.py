205695a93044f0681a60740b992845d018ca5487
N1XA-CLI <prasadvikram094@gmail.com>
2026-01-25 17:30:37 +0530

    Updated

--- Recursive_Delete/main.py
+++ Recursive_Delete/main.py
@@ -1,30 +1,35 @@
 import os
+import sys
 
 def handle_file(filelist, root_dir):
-    rm_files = ["IMPORTANT.txt", "README.txt", "README.url"]
+    rm_files = [] # Lists of files to delete
     for filename in filelist:
         if filename in rm_files:
             print(f"[+] Deleting {filename}")
             os.remove(os.path.join(root_dir, filename))
 
-def handle_main(root_dir):
+def handle_dir(root_dir):
     total_files = []
     for item in os.listdir(root_dir):
         path = os.path.join(root_dir, item)
         if os.path.isfile(path):
             total_files.append(item)
         elif os.path.isdir(path):
-            handle_main(path)
+            handle_dir(path)
     if not total_files:
         print(f"[-] File not found")
     else:
         handle_file(total_files, root_dir)
             
 def main():
-    dir_path = os.path.expanduser(r"~/Telegram Desktop")
-    handle_main(dir_path)
+    dir_path = os.path.expanduser(rf'{sys.argv[1]}')
+    handle_dir(dir_path)
 
 if __name__ == "__main__":
+    if not len(sys.argv) >= 2:
+        print("[!] Argument not found!")
+        print(f"[+] Usage: {__file__} <dir>")
+        sys.exit(404)
     print("[!] Started to look...")
     main()
     print("[+] Done!")
