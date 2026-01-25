78be59a378ed6cd79c4eaa13a56865b1c3fdab40
N1XA-CLI <prasadvikram094@gmail.com>
2026-01-25 11:04:27 +0530

    Progressed

--- Pong_Game/main.py
+++ Pong_Game/main.py
@@ -1,4 +1,4 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from paddel import Paddel
 
 screen = Screen()
@@ -7,23 +7,20 @@ screen.setup(width=800, height=600)
 screen.title("Pong Game")
 screen.tracer(0)
 
-paddel_left = Paddel()
-paddel_left.goto(350, 0)
+l_paddel = Paddel(position=(350, 0))
+r_paddel = Paddel(position=(-350, 0))
 
-paddel_right = Paddel()
-paddel_right.goto(-350, 0)
+screen.listen()
+screen.onkey(r_paddel.go_up, "Up")
+screen.onkey(r_paddel.go_down, "Down")
+screen.onkey(l_paddel.go_up, "w")
+screen.onkey(l_paddel.go_down, "s")
+
 
 game_is_on = True
 
 while game_is_on:
     screen.update()
-    
-    screen.listen()
-    screen.onkey(paddel_right.go_up, "Up")
-    screen.onkey(paddel_right.go_down, "Down")
-
-    screen.onkey(paddel_left.go_up, "w")
-    screen.onkey(paddel_left.go_down, "s")
 
 
 screen.exitonclick()
--- Pong_Game/paddel.py
+++ Pong_Game/paddel.py
@@ -2,18 +2,19 @@ from turtle import Turtle
 
 class Paddel(Turtle):
 
-    def __init__(self):
+    def __init__(self, position):
         """Creates a paddel."""
         super().__init__()
         self.shape("square")
         self.color("white")
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
+        self.goto(position)
         
     def go_up(self):
         """Vertically moves the paddel up by 20px."""
-        self.goto(self.xcor(), self.ycor() + 20)
+        self.goto(self.xcor(), (self.ycor() + 20))
         
     def go_down(self):
         """Vertically moves the paddel down by 20px."""
-        self.goto(self.xcor(), self.ycor() - 20)
+        self.goto(self.xcor(), (self.ycor() - 20))
