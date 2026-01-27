c421e691a8c61b5820df715bcd49181db5799524
N1XA-CLI <prasadvikram094@gmail.com>
2026-01-27 22:40:44 +0530

    Fixed from tutorial
    Wasn't wrong just a bit for better workflow

--- turtle-crossing-start/car_manager.py
+++ turtle-crossing-start/car_manager.py
@@ -6,20 +6,25 @@ STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 5
 MAX_CAR = 20
 
-class CarManager(Turtle):
+class CarManager():
     def __init__(self):
-        super().__init__()
-        self.shape("square")
-        self.color(random.choice(COLORS))
-        self.shapesize(stretch_len=2)
-        self.setheading(180)
-        self.penup()
-        self.y_axis = random.randint(-220, 220)
-        self.goto((270, self.y_axis))
-        
-    def move(self):
-        self.forward(STARTING_MOVE_DISTANCE)
+        self.all_cars = []
     
-    def level_up(self):
-        new_y = STARTING_MOVE_DISTANCE + MOVE_INCREMENT
-        self.goto((self.xcor() + new_y, self.ycor()))
+    def create_car(self):
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle(shape="square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            random_y = random.randint(-220, 220)
+            new_car.goto((300, random_y))
+            self.all_cars.append(new_car)
+        
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(STARTING_MOVE_DISTANCE)
+    
+    # def level_up(self):
+    #     new_y = STARTING_MOVE_DISTANCE + MOVE_INCREMENT
+    #     car.backward(STARTING_MOVE_DISTANCE)
