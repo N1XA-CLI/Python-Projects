f46c057fd9b8e363630356aa6ac853aa4d277d65
N1XA-CLI <prasadvikram094@gmail.com>
2026-01-28 18:05:26 +0530

    Completed Snake Gameee...

--- Snake_Game/scoreboard.py
+++ Snake_Game/scoreboard.py
@@ -8,7 +8,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = self.read_score()
         self.hideturtle()
         self.penup()
         self.color("white")
@@ -16,9 +16,7 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
-        self.read_score()
         self.write(arg=f"Score: {self.score} High Score {self.high_score}", align=ALIGNMENT, font=FONT)
-        print(self.high_score)
 
     def increase_scoreboard(self):
         self.score += 1
@@ -32,11 +30,12 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def write_score(self):
-        with open("high_score.txt", "w") as h_score:
-            h_score.write(self.high_score)
-
-    def read_score(self):
-        with open("high_score.txt", "r") as c_score:
-            self.high_score = c_score.read()
-
+        with open("data.txt", "w") as h_score:
+            h_score.write(str(self.high_score))
     
+    def read_score(self):
+        with open("data.txt", "r") as r_score:
+            currently_high_score = r_score.read()
+            return int(currently_high_score)
+            
+
