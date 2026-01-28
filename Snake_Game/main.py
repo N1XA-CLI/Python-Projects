baaae140c2cf43b0c7bd1d2bea7d784f496b4c97
N1XA-CLI <prasadvikram094@gmail.com>
2026-01-28 17:42:43 +0530

    Added

--- Snake_Game/main.py
+++ Snake_Game/main.py
@@ -19,7 +19,7 @@ screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
-scoreboard.start_scoreboard()
+scoreboard.update_scoreboard()
 
 
 game_is_on = True
@@ -34,13 +34,14 @@ while game_is_on:
         food.refresh()
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
+
 
 
 screen.exitonclick()
--- Snake_Game/scoreboard.py
+++ Snake_Game/scoreboard.py
@@ -1,6 +1,6 @@
 from turtle import Turtle
 ALIGNMENT = 'center'
-FONT = ('Times New Roman', 24, 'bold')
+FONT = ('Times New Roman', 20, 'bold')
 
 
 class Scoreboard(Turtle):
@@ -8,19 +8,35 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.hideturtle()
         self.penup()
         self.color("white")
         self.goto(0, 260)
 
-    def start_scoreboard(self):
-        self.write(arg=f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+    def update_scoreboard(self):
+        self.clear()
+        self.read_score()
+        self.write(arg=f"Score: {self.score} High Score {self.high_score}", align=ALIGNMENT, font=FONT)
+        print(self.high_score)
 
     def increase_scoreboard(self):
-        self.clear()
         self.score += 1
-        self.start_scoreboard()
+        self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(arg="GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.write_score()
+        self.score = 0
+        self.update_scoreboard()
+
+    def write_score(self):
+        with open("high_score.txt", "w") as h_score:
+            h_score.write(self.high_score)
+
+    def read_score(self):
+        with open("high_score.txt", "r") as c_score:
+            self.high_score = c_score.read()
+
+    
