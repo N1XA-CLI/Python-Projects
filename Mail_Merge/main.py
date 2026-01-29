b8e921767de2508924bfb3fcc90286ae0d6d5ff9
N1XA-CLI <prasadvikram094@gmail.com>
2026-01-29 16:07:11 +0530

    Mail_Merge Added and completed

---
+++ Mail_Merge/main.py
@@ -0,0 +1,21 @@
+#TODO: Create a letter using starting_letter.txt 
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+
+import os
+
+with open("./Input/Letters/starting_letter.txt") as letter:
+    with open("./Input/Names/invited_names.txt") as names:
+        for name in names.readlines():
+            name = name.rstrip("\n")
+            ready_letter = f"""Dear {name},
+
+You are invited to my birthday this Saturday.
+
+Hope you can make it!
+
+Angela"""
+            with open(f"./Output/ReadyToSend/letter_for_{name}.txt", "w") as letter:
+                letter.write(ready_letter)
+
