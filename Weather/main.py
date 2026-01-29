920b716693d98396efdb4de87179f2e1e4a4ca7a
N1XA-CLI <prasadvikram094@gmail.com>
2026-01-29 16:23:00 +0530

    Progressed

--- Weather/main.py
+++ Weather/main.py
@@ -1,6 +1,12 @@
+import csv
 import pandas
 
 with open("weather_data.csv") as weather_report:
-    reports = [report.strip() for report in weather_report.readlines()]
-    print(reports)
+    weekly_report = csv.reader(weather_report)
+    tempratures = []
+    for day_report in weekly_report:
+        if day_report[1] != "temp":
+            tempratures.append(int(day_report[1]))
+
+    print(tempratures)
     
