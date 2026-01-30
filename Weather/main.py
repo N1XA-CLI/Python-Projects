7bd102aefcddfc8d58e73d53d1ba11258c0c3fcb
N1XA-CLI <prasadvikram094@gmail.com>
2026-01-30 16:17:26 +0530

    Weather_Completed

--- Weather/main.py
+++ Weather/main.py
@@ -1,5 +1,5 @@
-import csv
-import pandas
+# import csv
+# import pandas
 
 # with open("weather_data.csv") as weather_report:
 #     weekly_report = csv.reader(weather_report)
@@ -9,10 +9,44 @@ import pandas
 #             tempratures.append(int(day_report[1]))
 #     print(tempratures)
 
-report = pandas.read_csv("weather_data.csv")
+# report = pandas.read_csv("weather_data.csv")
 # print(type(report["temp"]))
 # pandas.DataFrame.to_excel(report, excel_writer="weather_report.xlsx")
-temp_list = report["temp"].to_list()
-print(temp_list)
-avg_temp = sum(temp_list) / len(temp_list)
-print(f"Average Temperature: {avg_temp}")
+# temp_list = report["temp"]
+# print(temp_list.max())
+# avg_temp = sum(temp_list) / len(temp_list)
+# avg_temp = report["temp"].mean()
+# print(f"Average Temperature: {avg_temp}")
+# print(report[report.day == "Monday"]) 
+# print(report[report.temp == temp_list.max()])
+# monday = report[report.day == "Monday"]
+# print(monday.temp * 1.8 + 32)
+
+# dict_data = {
+#     "students": ["Amy", "N1XA", "Angela"],
+#     "score": [75, 23, 86]
+# }
+
+# print(dict_data)
+# data = pandas.DataFrame(dict_data)
+# data.to_csv("new_data.csv")
+# print(data)
+
+import pandas
+# Main Work
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+gray_squirrel_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrel_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrel_count = len(data[data["Primary Fur Color"] == "Black"])
+print(gray_squirrel_count)
+print(red_squirrel_count)
+print(black_squirrel_count)
+
+data_color_dict = {
+    "Fur Color": ["Gray", "Red", "Black"],
+    "Count": [gray_squirrel_count, red_squirrel_count, black_squirrel_count]
+}
+
+df = pandas.DataFrame(data_color_dict)
+df.to_csv("squirrel_count.csv")
